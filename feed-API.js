57fb5135e65a924425afdb7a6e2bb8530125cc2d
Xurde <xurdebrandulas@gmail.com>
2023-03-31 12:34:47 +0200

    add button like to posts

--- feed-API.js
+++ feed-API.js
@@ -21,16 +21,39 @@ fetch('https://jsonplaceholder.typicode.com/comments')
       const cardImg = document.createElement('img');
       cardImg.classList.add('card-img-top');
       cardImg.src = 'https://random.imagecdn.app/500/250';
-      cardImg.classList.add ("rounded-3", "d-flex")
+      cardImg.classList.add ("rounded-3", "d-flex");
 
+
+      const buttons = document.createRange().createContextualFragment(` 
+   
+      <div class="hstack gap-4 mt-3">
+      <!-- Boton Like -->
+        <button type="button" class="btn btn-outline-danger">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart-fill"  id="boton_likes" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path>
+          </svg>
+          <p id="contador-likes">0 </p>
+        </button>
+        <!-- Boton responder -->
+        <button class="nav-link border-0 bg-transparent" id="boton_responder" type="submit" >
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
+            <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
+          </svg>
+        </button>
+            
+
+        `);
+      
+      
       cardBody.appendChild(cardTitle);
       cardBody.appendChild(cardText);
       cardBody.appendChild(cardImg);
+      cardBody.appendChild(buttons);
       card.appendChild(cardBody);
 
-      card.addEventListener('click', () => {
-        window.location.href = `./index-responsive.html?id=${comment.id}`;
-      });
+      // card.addEventListener('click', () => {
+      //   window.location.href = `./index-responsive.html?id=${comment.id}`;
+      // });
 
       aplicacion.appendChild(card);
     });
