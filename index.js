217fe5efe8a4b9c9dca711024657c3d23b11f53a
DavidCoAr <127534314+DavidCoAr@users.noreply.github.com>
2023-04-11 19:27:38 +0200

    Funcionalidad en index.responsive
    -El botón de login al pulsar en él, te lleva a feed-responsive
    -El texto registrarse  al pulsar en él, te lleva a mi_perfil-responsive

---
+++ index.js
@@ -0,0 +1,15 @@
+//Vamos a llamar/obtener el boton de login del DOM(árbol de nodos)  por su ID
+    // document.getElementById("id del elemento")
+        //id="boton-login" 
+     
+const botonLogin = document.getElementById("boton-login");
+
+// Agregar un evento de clic al botón "Login" que me llevará a la pantalla de Feed
+    //Sintaxis element.addEventListener(event, function, useCapture);
+        // event es es tipo de evento como "click" 
+        // function es la funcion que llamaremos cuando el evento ocurra
+        // useCapture es OPCIONAL, valor booleano que especifica si usar event bubbling o event capturing 
+
+botonLogin.addEventListener("click", function() {
+  window.location.href = "feed-responsive.html"
+});
