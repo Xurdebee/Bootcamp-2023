21223e9b5135733579febb5fe093e093e99eb656
Rociogvijande <126900560+Rociogvijande@users.noreply.github.com>
2023-04-26 11:55:31 +0200

    quite ccs old

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 //Vamos a llamar/obtener el boton de login del DOM(árbol de nodos)  por su ID
     // document.getElementById("id del elemento")
         //id="boton-login" 
-     
-const botonLogin = document.getElementById("boton-login");
+// const botonLogin = document.getElementById("boton-login");
 
 // Agregar un evento de clic al botón "Login" que me llevará a la pantalla de Feed
     //Sintaxis element.addEventListener(event, function, useCapture);
