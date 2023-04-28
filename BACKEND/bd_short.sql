10be80e3ce68211cc2c4a1fc4bbe204a2662f7cd
Xurde <xurdebrandulas@gmail.com>
2023-04-28 15:16:24 +0200

    database

--- BACKEND/bd_short.sql
+++ BACKEND/bd_short.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-04-2023 a las 19:13:11
+-- Tiempo de generación: 28-04-2023 a las 15:15:29
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -108,24 +108,25 @@ CREATE TABLE `users` (
   `country` varchar(50) NOT NULL,
   `city` varchar(50) NOT NULL,
   `linkedIn` varchar(150) NOT NULL,
-  `education` varchar(50) NOT NULL
+  `education` varchar(50) NOT NULL,
+  `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`user_id`, `alias`, `name`, `surname`, `email`, `password`, `birthday`, `country`, `city`, `linkedIn`, `education`) VALUES
-(1, 'Xurderoc', 'Xurde', 'Brándulas Pellitero', 'xurderocbrandulas@gmail.com', 1234, '1991-03-04', 'España', 'Gijón', 'linkedin.com/in/xurderoc', 'Ingeniería en agricultura espacial'),
-(2, 'TechGeek92', 'Juan', 'Pérez', 'agonzalez@gmail.com', 12341, '1985-08-10', 'México', 'Ciudad de México', 'linkedin.com/in/alejandrogonzalez', ' Licenciatura en Administración de Empresas'),
-(3, 'TechGeek', 'Sofía', 'García', 'sofia.garcia@hotmail.com', 12342, '1992-03-22', 'España', 'Barcelona', 'linkedin.com/in/sofiagarcia', 'Grado en Ingeniería Informática'),
-(4, 'TheArtist', 'TheArtist', 'Rodríguez', 'camilarodriguez@gmail.com', 12343, '1998-11-02', 'Argentina', 'Buenos Aires', 'linkedin.com/in/camilarodriguez', 'Licenciatura en Bellas Artes'),
-(5, 'TheAthlete', 'Juan', 'López', 'juan.lopez@hotmail.com', 12344, '1996-06-14', 'Colombia', 'Bogotá', 'linkedin.com/in/juanlopez', 'Licenciatura en Educación Física'),
-(6, 'TheWriter', 'TheWriter', 'Fernández', 'diegofernandez@yahoo.com', 12345, '1987-12-05', 'México', 'Guadalajara', 'linkedin.com/in/diegofernandez', 'Licenciatura en Letras Hispánicas'),
-(7, 'TheDoctor', 'Ana', 'Pérez', 'anaperez@gmail.com', 12346, '1983-04-11', 'España', 'Madrid', 'linkedin.com/in/anaperez', 'Licenciatura en Medicina'),
-(8, 'TheDesigner', 'Francisco', 'Francisco', 'franciscoreyes@yahoo.com', 12347, '1994-07-27', 'Argentina', 'Córdoba', 'linkedin.com/in/franciscoreyes', 'Licenciatura en Diseño Gráfico'),
-(9, 'TheTeacher', 'Gabriela', 'Morales', 'gabrielamorales@hotmail.com', 12348, '1991-02-14', 'México', 'Monterrey', 'linkedin.com/in/gabrielamorales', 'Licenciatura en Pedagogía'),
-(10, 'TheConsultant', 'Alejandro', 'García', 'alejandrogarcia@gmail.com', 12349, '1989-09-03', 'Colombia', 'Medellín', 'linkedin.com/in/alejandrogarcia', 'Maestría en Administración');
+INSERT INTO `users` (`user_id`, `alias`, `name`, `surname`, `email`, `password`, `birthday`, `country`, `city`, `linkedIn`, `education`, `image`) VALUES
+(1, 'Exterminadora de aliens', 'Ellen', 'Ripley', 'alien@gmail.com', 1234, '2092-01-07', 'Estados Unidos', 'New York', 'linkedin.com/in/ripley', 'Fuerzas armadas', 'img/users/user_1.jpg'),
+(2, 'TechGeek92', 'Juan', 'Pérez', 'agonzalez@gmail.com', 12341, '1985-08-10', 'México', 'Ciudad de México', 'linkedin.com/in/alejandrogonzalez', ' Licenciatura en Administración de Empresas', 'img/users/user_2.jpg'),
+(3, 'TechGeek', 'Sofía', 'García', 'sofia.garcia@hotmail.com', 12342, '1992-03-22', 'España', 'Barcelona', 'linkedin.com/in/sofiagarcia', 'Grado en Ingeniería Informática', 'img/users/user_3.jpg'),
+(4, 'TheArtist', 'Camilo', 'Rodríguez', 'camilorodriguez@gmail.com', 12343, '1998-11-02', 'Argentina', 'Buenos Aires', 'linkedin.com/in/camilarodriguez', 'Licenciatura en Bellas Artes', 'img/users/user_4.jpg'),
+(5, 'TheAthlete', 'Juan', 'López', 'juan.lopez@hotmail.com', 12344, '1996-06-14', 'Colombia', 'Bogotá', 'linkedin.com/in/juanlopez', 'Licenciatura en Educación Física', 'img/users/user_5.jpg'),
+(6, 'TheWriter', 'Diego', 'Sánchez', 'diegosanchez@yahoo.com', 12345, '1987-12-05', 'México', 'Guadalajara', 'linkedin.com/in/diegosanchez', 'Licenciatura en Letras Hispánicas', 'img/users/user_6.jpg'),
+(7, 'TheDoctor', 'Ana', 'Pérez', 'anaperez@gmail.com', 12346, '1983-04-11', 'España', 'Madrid', 'linkedin.com/in/anaperez', 'Licenciatura en Medicina', 'img/users/user_7.jpg'),
+(8, 'TheDesigner', 'Francisco', 'Reyes', 'franciscoreyes@yahoo.com', 12347, '1994-07-27', 'Argentina', 'Córdoba', 'linkedin.com/in/franciscoreyes', 'Licenciatura en Diseño Gráfico', 'img/users/user_8.jpg'),
+(9, 'TheTeacher', 'Gabriela', 'Morales', 'gabrielamorales@hotmail.com', 12348, '1991-02-14', 'México', 'Monterrey', 'linkedin.com/in/gabrielamorales', 'Licenciatura en Pedagogía', 'img/users/user_9.jpg'),
+(10, 'TheConsultant', 'Alejandro', 'García', 'alejandrogarcia@gmail.com', 12349, '1989-09-03', 'Colombia', 'Medellín', 'linkedin.com/in/alejandrogarcia', 'Maestría en Administración', 'img/users/user_10.jpg');
 
 --
 -- Índices para tablas volcadas
