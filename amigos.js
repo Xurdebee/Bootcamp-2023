a7e8c94d797c50136fbcd24420518d5fffa77de6
Rociogvijande <126900560+Rociogvijande@users.noreply.github.com>
2023-05-07 21:20:31 +0200

    boton agregar/eliminar amigo sin funcionalidad

--- amigos.js
+++ amigos.js
@@ -1,35 +1,42 @@
-function getCharacter(done){
-    const results = fetch("https://rickandmortyapi.com/api/character/?page=2");
-      results
-          .then(response => response.json())
-          .then(data=>{
-              done(data)
-          .catch((err) => console.log(err));
-          });
-  }
-      
-  getCharacter(data=>{
-    data.results.forEach(personaje=>{
-      const amigos = document.createRange().createContextualFragment(` 
-      
-        <div class="m-3 col">
-          
+// Asumiendo que tienes una lista de amigos con una propiedad id, como ejemplo:
+const amigosList = [
+  { id: 1, name: " " },
+  { id: 2, name: " " }
+];
 
-            <div class="me-2">
-              <a href="#"><img class="rounded-circle" src="${personaje.image}" width="100" alt=""></img></a>
+function getCharacter(done) {
+  const results = fetch("https://rickandmortyapi.com/api/character/?page=2");
+  results
+    .then(response => response.json())
+    .then(data => {
+      done(data);
+    })
+    .catch(err => console.log(err));
+}
+
+getCharacter(data => {
+  data.results.forEach(personaje => {
+    const esAmigo = amigosList.some(amigo => amigo.id === personaje.id);
+    const actionButton = esAmigo
+      ? `<button class="btn btn-danger">Eliminar amigo</button>`
+      : `<button class="btn btn-primary">Agregar amigo</button>`;
+
+    const personajeHTML = document
+      .createRange()
+      .createContextualFragment(` 
+        <div class="m-3 col">
+          <div class="me-2">
+            <a href="#"><img class="rounded-circle" src="${personaje.image}" width="100" alt=""></img></a>
+          </div>
+          <div class="overflow-hidden ">
+            <a class="h6 mb-0" href="#!">${personaje.name}</a>
+            <p class="mb-0 small text-truncate">${personaje.species}</p>
+            ${actionButton }
             </div>
-              
-            <div class="overflow-hidden ">
-              <a class="h6 mb-0" href="#!">${personaje.name}</a>
-              <p class="mb-0 small text-truncate">${personaje.species}</p>
-            </div>
-          
-        </div>
-        
+          </div>
+        `);
   
-      `); 
-          const main =document.querySelector("article");
-          main.append(amigos);
-  
+      const main = document.querySelector("article");
+      main.append(personajeHTML);
     });
   });
