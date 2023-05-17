67f3032c2d23d6a355f4cb5318af96cdb16e4d4c
Xurde <xurdebrandulas@gmail.com>
2023-05-17 14:58:19 +0200

    Modificada elementos ruta App (externa a carpeta)

---
+++ REACT/mi-red-social/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import NavBar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import LoginForm from './components/Login/LoginForm';
+import ColumnaSugeridos from './components/Feed/ColumnaSugeridos';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ColumnaUsuario from './components/Feed/ColumnaUsuario';
+import AmigoSugerido from './components/Feed/AmigoSugerido';
+
+//no es necesario si no utilizamos los css para toda la web, estructura sacada de Silvia
+// import "./App.css";
+
+
+
+
+function App() {
+  return (
+    <div>
+
+      {/* dentro de esto cargamos las rutas que tengamos
+      
+      <>
+        <Routes>
+          <route path="/" element={<Login/>}/}
+          <route path="/feed" element={<Feed/>}/}
+        </Routes>
+       </>
+
+       */}
+    
+      <NavBar />
+      {/* <LoginForm/> */}
+      <div className="row mt-3">
+        <div className="col-lg-3">
+          <ColumnaUsuario/>
+        </div>
+        {/* <div className="col-lg-6">
+          <ColumnaPost/>
+        </div> */}
+        <div className="col-lg-3">
+          <ColumnaSugeridos><AmigoSugerido/></ColumnaSugeridos>
+        </div>
+      </div>
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
