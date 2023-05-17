ce176dfd4e24c9836f1305f8d423e0de12b3b380
Rociogvijande <126900560+Rociogvijande@users.noreply.github.com>
2023-05-17 18:06:01 +0200

    Rutas creadas en RouterPrincipal.js

--- REACT/mi-red-social/src/App.js
+++ REACT/mi-red-social/src/App.js
@@ -1,58 +1,17 @@
 // importar los componentes de react-router-dom
-import { BrowserRouter, Route, Link } from "react-router-dom";
-
-import React from 'react';
-
-import NavBar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import LoginForm from './components/Login/LoginForm';
-import ColumnaSugeridos from './components/Feed/ColumnaSugeridos';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ColumnaUsuario from './components/Feed/ColumnaUsuario';
-import AmigoSugerido from './components/Feed/AmigoSugerido';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { RouterPrincipal } from "./Router/RouterPrincipal";
 
 //no es necesario si no utilizamos los css para toda la web, estructura sacada de Silvia
 // import "./App.css";
 
-
-
-
 function App() {
   return (
     <div>
-
-      {/* dentro de esto cargamos las rutas que tengamos
-      
-      <>
-        <Routes>
-          <route path="/" element={<Login/>}/}
-          <route path="/feed" element={<Feed/>}/}
-        </Routes>
-       </>
-
-       */}
-    
-      <NavBar />
-      {/* <LoginForm/> */}
-      <div className="row mt-3">
-        <div className="col-lg-3">
-          <ColumnaUsuario/>
-        </div>
-        {/* <div className="col-lg-6">
-          <ColumnaPost/>
-        </div> */}
-        <div className="col-lg-3">
-          <ColumnaSugeridos><AmigoSugerido/></ColumnaSugeridos>
-        </div>
-      </div>
-
-      <Footer />
-
+      <RouterPrincipal />
     </div>
-
   );
 }
-
 
 export default App;
