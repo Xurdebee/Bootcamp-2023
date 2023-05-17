adffcbaf2d037e8b6daadebc09ebcb58a1a28122
DavidCoAr <127534314+DavidCoAr@users.noreply.github.com>
2023-05-17 10:56:43 +0200

    Creación del componente RegistroForm.js
    Instalado react-router-dom para el manejo de rutas

--- REACT/mi-red-social/src/App.js
+++ REACT/mi-red-social/src/App.js
@@ -1,8 +1,12 @@
+// importar los componentes de react-router-dom
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
 import React from 'react';
 import NavBar from './components/Navbar';
 import Footer from './components/Footer';
 import LoginForm from './components/LoginForm';
 import ColumnaSugeridos from './components/ColumnaSugeridos';
+import RegistroForm from './components/RegistroForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -14,10 +18,13 @@ function App() {
       <NavBar />
       {/* <LoginForm/> */}
       <ColumnaSugeridos/>
+      <RegistroForm/>
       <Footer />
+
     </div>
 
   );
 }
+
 
 export default App;
