557dcfe43a3ecd65754e7917d9826e5a9e7356ce
Xurde <xurdebrandulas@gmail.com>
2023-05-31 20:12:34 +0200

    Búsqueda de navbar funciona ahora con nombres, apellidos y alias

--- REACT/mi-red-social/src/components/UsuariosBusqueda/ListadoBusqueda.js
+++ REACT/mi-red-social/src/components/UsuariosBusqueda/ListadoBusqueda.js
@@ -3,67 +3,53 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const ListadoBusqueda = () => {
   const [searchResults, setSearchResults] = useState([]);
-  const [error, setError] = useState("");
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const alias = urlParams.get("alias");
+    const searchParams = new URLSearchParams(window.location.search);
+    const searchValue = searchParams.get("value");
 
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://localhost:3000/users/${alias}`);
-        const data = await response.json();
-
-        if (data.length > 0) {
-          setSearchResults(data);
-          setError("");
-        } else {
-          setSearchResults([]);
-          setError("Hay 0 coincidencias con los datos introducidos");
-        }
-      } catch (error) {
+    fetch(`http://localhost:3000/search/${searchValue}`)
+      .then((response) => response.json())
+      .then((user) => {
+        setSearchResults(user);
+      })
+      .catch((error) => {
         console.error(error);
-        setError("Error al ejecutar la consulta: " + error.message);
-      }
-    };
+      });
 
-    if (alias) {
-      fetchData();
-    }
   }, []);
 
   return (
     <Container>
       <div className="bg-light p-2 rounded-3 border-1 border">
-        <div className="text-center mb-3">
-      {error && <p>{error}</p>}
-
-      {searchResults.length > 0 && (
-        <Row>
-          {searchResults.map((user) => (
-            <Col key={user.id} xs={6} sm={4} md={3} lg={3} xxl={2}>
-              <div className="text-center mb-5">
-                {/*  Sustituir en href por URL del perfil del usuario */}
-                <a href={`/user/${user.user_id}`}>
-                  <img
-                    className="rounded-circle"
-                    src={user.image}
-                    width="50"
-                    alt=""
-                  />
-                </a>
-                <div>
-                  <a className="h6 mb-0" href={`/user/${user.user_id}`}>
-                    {user.name} {user.surname}
-                  </a>
-                  <p className="mb-2 small text-truncate">{user.alias}</p>
-                </div>
-              </div>
-            </Col>
-          ))}
-        </Row>
-      )}
-      </div>
+        <div className="text-center my-3">
+          {searchResults.length > 0 ? (
+            <Row>
+              {searchResults.map((user) => (
+                <Col key={user.user_id} xs={6} sm={4} md={3} lg={3} xxl={2}>
+                  <div className="text-center mb-5">
+                    <a href={`/user/${user.user_id}`}>
+                      <img
+                        className="rounded-circle"
+                        src={user.image}
+                        width="70"
+                        alt=""
+                      />
+                    </a>
+                    <div>
+                      <a className="h6 mb-0" href={`/user/${user.user_id}`}>
+                        {user.name} {user.surname}
+                      </a>
+                      <p className="mb-2 small text-truncate">{user.alias}</p>
+                    </div>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          ) : (
+            <b>No hay ningún usuario con esos datos.</b>
+          )}
+        </div>
       </div>
     </Container>
   );
